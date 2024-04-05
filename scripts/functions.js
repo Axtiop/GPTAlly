c43708f7819db26966b53e078ac6bf02238af0c6
Axtiop <brieuc.bastin@student.uclouvain.be>
2024-04-05 19:28:27 +0900

    Few updates, it has been a while :p

---
+++ scripts/functions.js
@@ -0,0 +1,39 @@
+let changeVideoBtn = document.getElementById("changeData");
+let video = document.getElementById('dataVideo');
+let newSource =  `data_functions/P1/P1/Video.mp4`;
+let promptsrc = 1;
+let pointCounter = 1;
+
+function changeVideoSource(i) {
+    promptsrc = i;
+    newSource = `data_functions/P${i}/P${pointCounter}/Video.mp4`;
+    video.src = newSource;
+    video.load();
+    restartVideo();
+}
+
+for (let i = 1; i <= 10; i++) {
+    let prompt = document.getElementById(`prompt${i}`);
+    console.log(`prompt${i}`)
+    prompt.addEventListener('click', function() {
+        changeVideoSource(i);
+    });
+}
+changeVideoBtn.addEventListener('click', function(event) {
+    event.preventDefault();
+    pointCounter = (pointCounter % 3 ) + 1;
+    changeVideoSource(promptsrc);
+});
+
+function playVideo() {
+    video.play();
+}
+
+function stopVideo() {
+    video.pause();
+}
+
+function restartVideo() {
+    video.currentTime = 0; // Set current time to 0 (start of the video)
+}
+
